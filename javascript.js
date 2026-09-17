eb8d1ee42cdc32384fe752831eef15726864868d
Isaac Severo <isaaccamargosevero@gmail.com>
2026-09-17 14:23:55 -0400

    IMC

--- javascript.js
+++ javascript.js
@@ -1,13 +1,21 @@
-// FizzBuzz - JavaScript
+// Calculadora de IMC - JavaScript
 
-for (let numero = 1; numero <= 100; numero++) {
-    if (numero % 3 === 0 && numero % 5 === 0) {
-        console.log("FizzBuzz");
-    } else if (numero % 3 === 0) {
-        console.log("Fizz");
-    } else if (numero % 5 === 0) {
-        console.log("Buzz");
-    } else {
-        console.log(numero);
-    }
+const peso = Number(prompt("Digite seu peso em kg:"));
+const altura = Number(prompt("Digite sua altura em metros:"));
+
+const imc = peso / (altura ** 2);
+
+let classificacao;
+
+if (imc < 18.5) {
+    classificacao = "abaixo do peso";
+} else if (imc < 25) {
+    classificacao = "normal";
+} else if (imc < 30) {
+    classificacao = "sobrepeso";
+} else {
+    classificacao = "obesidade";
 }
+
+console.log(`IMC: ${imc.toFixed(2)}`);
+console.log(`Classificação: ${classificacao}`);
